e9f1569f61e6338a691c01415e71ab4dc8d00bbd
sanchitaochani <so99039n@pace.edu>
2019-07-27 03:45:05 -0400

    Layout

--- app/src/main/java/axxess/appdev/example/android/codingchallenge/Items.java
+++ app/src/main/java/axxess/appdev/example/android/codingchallenge/Items.java
@@ -2,8 +2,18 @@ package axxess.appdev.example.android.codingchallenge;
 
 class Items {
 
+    private int imageID;
+
+    public Items(int img) {
+        imageID = img;
+    }
+
+    public void setImage(int image_id) {
+        imageID = image_id;
+    }
 
     public int getImage() {
-        return 0;
+        return imageID;
     }
+
 }
